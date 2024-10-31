10d67757e65527d6f9f9d08a337199af97d46736
lostsys <67213914+lostsys@users.noreply.github.com>
2024-10-31 11:55:25 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,136 @@
+let replacements = {};
+
+// ANTICHEAT HOOK
+function replaceAndCopyFunction(oldFunc, newFunc) {
+	return new Proxy(oldFunc, {
+		apply(orig, origIden, origArgs) {
+			const result = orig.apply(origIden, origArgs);
+			newFunc(result);
+			return result;
+		},
+		get(orig) { return orig; }
+	});
+}
+
+Object.getOwnPropertyNames = replaceAndCopyFunction(Object.getOwnPropertyNames, function(list) {
+	if (list.indexOf(storeName) != -1) list.splice(list.indexOf(storeName), 1);
+	return list;
+});
+Object.getOwnPropertyDescriptors = replaceAndCopyFunction(Object.getOwnPropertyDescriptors, function(list) {
+	delete list[storeName];
+	return list;
+});
+
+function addReplacement(replacement, code, replaceit) {
+	replacements[replacement] = [code, replaceit];
+}
+
+function addDump(replacement, code) {
+	dumpedVarNames[replacement] = code;
+}
+
+function modifyCode(text) {
+	for(const [name, regex] of Object.entries(dumpedVarNames)){
+		const matched = text.match(regex);
+		if (matched) {
+			console.log(name, regex, matched);
+			for(const [replacement, code] of Object.entries(replacements)){
+				delete replacements[replacement];
+				replacements[replacement.replaceAll(name, matched[1])] = [code[0].replaceAll(name, matched[1]), code[1]];
+			}
+		}
+	}
+
+	for(const [replacement, code] of Object.entries(replacements)){
+		text = text.replaceAll(replacement, code[1] ? code[0] : replacement + code[0]);
+	}
+
+	var newScript = document.createElement("script");
+	newScript.type = "module";
+	newScript.crossOrigin = "";
+	newScript.textContent = text;
+	var head = document.querySelector("head");
+	head.appendChild(newScript);
+	newScript.textContent = "";
+	newScript.remove();
+}
+
+addReplacement('async loadSpritesheet(){', `
+    async loadSpritesheet() {
+        // SPRITESHEET URL
+        const customSpritesheet = await this.loader.loadAsync("https://path-to-your-spritesheet/spritesheet.png");
+        const canvas = document.createElement("canvas");
+
+        canvas.width = SPRITE_WIDTH;
+        canvas.height = SPRITE_HEIGHT;
+
+        const ctx = canvas.getContext("2d");
+        ctx.drawImage(customSpritesheet.image, 0, 0);
+
+        this.spritesheetPixels = ctx.getImageData(0, 0, SPRITE_WIDTH, SPRITE_HEIGHT).data;
+
+        customSpritesheet.minFilter = NearestFilter;
+        customSpritesheet.magFilter = NearestFilter;
+        customSpritesheet.colorSpace = LinearSRGBColorSpace;
+
+        const textureMapping = {
+            map: customSpritesheet,
+            side: FrontSide,
+            transparent: false,
+            depthWrite: true,
+            alphaTest: 0.1,
+            vertexColors: true
+        };
+
+        this.customTextureMapping = textureMapping;
+    }
+`, true);
+
+
+(function() {
+	'use strict';
+
+	// PRE
+	addReplacement('document.addEventListener("DOMContentLoaded",startGame,!1);', `
+		setTimeout(function() {
+			var DOMContentLoaded_event = document.createEvent("Event");
+			DOMContentLoaded_event.initEvent("DOMContentLoaded", true, true);
+			document.dispatchEvent(DOMContentLoaded_event);
+		}, 0);
+	`);
+
+	addReplacement('SliderOption("Render Distance ",2,8,3)', 'SliderOption("Render Distance ",2,64,3)', true);
+
+	const publicUrl = "scripturl";
+	// https://stackoverflow.com/questions/22141205/intercept-and-alter-a-sites-javascript-using-greasemonkey
+	if (publicUrl == "scripturl") {
+		if (navigator.userAgent.indexOf("Firefox") != -1) {
+			window.addEventListener("beforescriptexecute", function(e) {
+				if (e.target.src.includes("https://miniblox.io/assets/index")) {
+					e.preventDefault();
+					e.stopPropagation();
+					execute(e.target.src);
+				}
+			}, false);
+		}
+		else {
+			new MutationObserver(async (mutations, observer) => {
+				let oldScript = mutations
+					.flatMap(e => [...e.addedNodes])
+					.filter(e => e.tagName == 'SCRIPT')
+					.find(e => e.src.includes("https://miniblox.io/assets/index"));
+
+				if (oldScript) {
+					observer.disconnect();
+					execute(oldScript.src, oldScript);
+				}
+			}).observe(document, {
+				childList: true,
+				subtree: true,
+			});
+		}
+	}
+	else {
+		execute(publicUrl);
+	}
+})();
