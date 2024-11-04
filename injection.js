0f084f5726b5ee35074d55c01613791616b718d0
lostsys <67213914+lostsys@users.noreply.github.com>
2024-11-04 12:38:23 -0500

    Update injection.js

--- injection.js
+++ injection.js
@@ -30,9 +30,9 @@ function modifyCode(text) {
 		}, 0);
 	`);
 	addReplacement('this.loader.loadAsync("textures/spritesheet.png")', 'this.loader.loadAsync("https://raw.githubusercontent.com/lostsys/sc-tes/main/spritesheet.png")', true);
-	addReplacement('SliderOption("Render Distance ",2,8,3)', 'SliderOption("Render Distance ",2,24,3)', true);
+	addReplacement('SliderOption("Render Distance ",2,8,3)', 'SliderOption("Render Distance ",2,32,3)', true);
 	addReplacement('mt.enabled === !1', 'mt.enabled === !1', true);
-	addReplacement('mt = this; this.enabled = !1,', 'mt = this; this.enabled = !0,', true);
+	addReplacement('this.enabled = !1,', 'this.enabled = !0,', true);
 
 	async function execute(src, oldScript) {
 		if (oldScript) oldScript.type = 'javascript/blocked';
